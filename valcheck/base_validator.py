8b80d5a7435683f72c53bf9b859b9851454e8041
Nishant Rao <nishant.rao173@gmail.com>
2022-09-10 15:17:03 +0530

    revamped entire error model and it's usage

--- valcheck/base_validator.py
+++ valcheck/base_validator.py
@@ -6,22 +6,28 @@ from valcheck.models import Error
 from valcheck.utils import (
     is_empty,
     set_as_empty,
+    wrap_in_quotes_if_string,
 )
 
 
 class BaseValidator:
-    def __init__(
-            self,
-            *,
-            data: Optional[Dict[str, Any]] = None,
-        ) -> None:
-        if data is not None:
-            assert isinstance(data, dict), "Param `data` must be a dictionary"
-        self.data = set_as_empty() if data is None else data
-        self._field_validators_dict = self._get_field_validators_dict()
-        self._model_validators = self._get_model_validators()
-        self._errors = []
-        self._validated_data = {}
+    """
+    Exposed properties:
+        - errors
+        - errors_as_list_of_dicts
+        - validated_data
+
+    Exposed methods:
+        - is_valid()
+        - list_validators()
+    """
+    def __init__(self, *, data: Dict[str, Any]) -> None:
+        assert isinstance(data, dict), "Param `data` must be a dictionary"
+        self.data = data
+        self._field_validators_dict: Dict[str, BaseField] = self._get_field_validators_dict()
+        self._model_validators: List[Callable] = self._get_model_validators()
+        self._errors: List[Error] = []
+        self._validated_data: Dict[str, Any] = {}
 
     def _get_field_validators_dict(self) -> Dict[str, BaseField]:
         """Returns dictionary having keys = field names, and values = field validator instances"""
@@ -40,34 +46,26 @@ class BaseValidator:
             validator_func for _, validator_func  in vars(self.__class__).items() if callable(validator_func)
         ]
 
-    @staticmethod
-    def _wrap_in_quotes_if_string(obj: Any) -> str:
-        if isinstance(obj, str):
-            return f"'{obj}'"
-        return f"{obj}"
-
     @property
-    def errors(self) -> List[Dict[str, Any]]:
+    def errors(self) -> List[Error]:
         return self._errors
 
-    def clear_errors(self) -> None:
+    @property
+    def errors_as_list_of_dicts(self) -> List[Dict[str, Any]]:
+        return [error.as_dict() for error in self.errors]
+
+    def _clear_errors(self) -> None:
         """Clears out the list of errors"""
-        self._errors = []
+        self._errors.clear()
 
-    def _update_error_kwargs(
-            self,
-            error_kwargs: Dict[str, Any],
-            kwarg: Dict[str, Any],
-        ) -> None:
-        """Updates the `error_kwargs` dictionary with the `kwarg` given (in-place)"""
-        error_kwargs.update(**kwarg)
+    def _register_error(self, error: Error) -> None:
+        self._errors.append(error)
 
-    def _register_error(self, error_kwargs: Dict[str, Any]) -> None:
-        self._errors.append(Error(**error_kwargs).as_dict())
+    def _assign_validator_message_to_error(self, *, error: Error, validator_message: str) -> None:
+        error.validator_message = validator_message
 
     def _register_validated_data(self, field: str, field_value: Any) -> None:
-        if not is_empty(field_value):
-            self._validated_data[field] = field_value
+        self._validated_data[field] = field_value
 
     def _unregister_validated_data(self, field: str) -> None:
         self._validated_data.pop(field, None)
@@ -76,15 +74,9 @@ class BaseValidator:
     def validated_data(self) -> Dict[str, Any]:
         return self._validated_data
 
-    def clear_validated_data(self) -> None:
+    def _clear_validated_data(self) -> None:
         """Clears out the dictionary having validated data"""
-        self._validated_data = {}
-
-    def _ensure_validator_message_is_missing(self, error_kwargs: Dict[str, Any]) -> None:
-        """Ensures that the kwarg 'validator_message' is not passed in `error_kwargs`, as this will be set by `BaseValidator`"""
-        assert ('validator_message' not in error_kwargs), (
-            "The param `error_kwargs` must not have the kwarg 'validator_message', as this will be set by `BaseValidator`"
-        )
+        self._validated_data.clear()
 
     def _perform_field_validation_checks(
             self,
@@ -94,19 +86,18 @@ class BaseValidator:
         ) -> None:
         """Performs validation checks for the given field, and registers errors (if any)"""
         required = field_validator_instance.required
-        error_kwargs = field_validator_instance.error_kwargs
+        error = field_validator_instance.error
         default_func = field_validator_instance.default_func
         default_value = default_func() if default_func is not None and not required else set_as_empty()
-        self._ensure_validator_message_is_missing(error_kwargs=error_kwargs)
         field_type = field_validator_instance.__class__.__name__
         field_value = self.data.get(field, default_value)
         MISSING_FIELD_ERROR_MESSAGE = f"Missing {field_type} '{field}'"
-        INVALID_FIELD_ERROR_MESSAGE = f"Invalid {field_type} '{field}' having value {self._wrap_in_quotes_if_string(field_value)}"
+        INVALID_FIELD_ERROR_MESSAGE = f"Invalid {field_type} '{field}' having value {wrap_in_quotes_if_string(field_value)}"
         self._register_validated_data(field=field, field_value=field_value)
         if is_empty(field_value) and required:
             self._unregister_validated_data(field=field)
-            self._update_error_kwargs(error_kwargs=error_kwargs, kwarg={'validator_message': MISSING_FIELD_ERROR_MESSAGE})
-            self._register_error(error_kwargs=error_kwargs)
+            self._assign_validator_message_to_error(error=error, validator_message=MISSING_FIELD_ERROR_MESSAGE)
+            self._register_error(error=error)
             return
         if is_empty(field_value) and not required:
             self._unregister_validated_data(field=field)
@@ -114,29 +105,29 @@ class BaseValidator:
         field_validator_instance.field_value = field_value
         if not field_validator_instance.is_valid():
             self._unregister_validated_data(field=field)
-            self._update_error_kwargs(error_kwargs=error_kwargs, kwarg={'validator_message': INVALID_FIELD_ERROR_MESSAGE})
-            self._register_error(error_kwargs=error_kwargs)
+            self._assign_validator_message_to_error(error=error, validator_message=INVALID_FIELD_ERROR_MESSAGE)
+            self._register_error(error=error)
             return
         return None
 
     def _perform_model_validation_checks(self, *, model_validator: Callable) -> None:
         """Performs validation checks for the given `model_validator`, and registers errors (if any)"""
-        error_kwargs = model_validator(self._validated_data.copy())
-        assert (error_kwargs is None or isinstance(error_kwargs, dict)), (
-            "Output of model validator functions should be either NoneType or a dictionary having error kwargs"
+        error = model_validator(self._validated_data.copy())
+        assert error is None or isinstance(error, Error), (
+            "Output of model validator functions should be either a NoneType or an instance of `valcheck.models.Error`"
         )
-        if error_kwargs is None:
+        if error is None:
             return None
-        self._ensure_validator_message_is_missing(error_kwargs=error_kwargs)
         INVALID_MODEL_ERROR_MESSAGE = f"Invalid model due to failed validation in `{model_validator.__name__}()`"
-        self._update_error_kwargs(error_kwargs=error_kwargs, kwarg={'validator_message': INVALID_MODEL_ERROR_MESSAGE})
-        self._register_error(error_kwargs=error_kwargs)
+        self._assign_validator_message_to_error(error=error, validator_message=INVALID_MODEL_ERROR_MESSAGE)
+        self._register_error(error=error)
         return None
 
     def _raise_exception_if_needed(self, raise_exception: bool, many: bool) -> None:
         """Raises `ValidationError` if needed"""
         if raise_exception and self.errors:
-            raise ValidationError(error_info=self.errors) if many else ValidationError(error_info=self.errors[0])
+            errors = self.errors_as_list_of_dicts
+            raise ValidationError(error_info=errors) if many else ValidationError(error_info=errors[0])
 
     def is_valid(
             self,
@@ -145,9 +136,8 @@ class BaseValidator:
             many: Optional[bool] = True,
         ) -> bool:
         """Returns boolean. If `raise_exception=True` and data validation fails, then raises `ValidationError`"""
-        assert isinstance(self.data, dict), "Cannot call `is_valid()` without setting the `data` dictionary"
-        self.clear_errors()
-        self.clear_validated_data()
+        self._clear_errors()
+        self._clear_validated_data()
         for field, field_validator_instance in self._field_validators_dict.items():
             self._perform_field_validation_checks(
                 field=field,
