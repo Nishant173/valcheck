03cebcb310da1977ab5d16c97b8d522de9cd2066
Nishant Rao <nishant.rao173@gmail.com>
2024-10-06 15:15:02 +0530

    updated validators

--- valcheck/apis/validators.py
+++ valcheck/apis/validators.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Any, Optional
 
 from valcheck.apis import status_codes
 from valcheck.apis.exceptions import ApiRequestValidationException
@@ -8,17 +8,19 @@ from valcheck.validators import Validator
 class ApiRequestValidator(Validator):
     """Class that represents an API request Validator."""
 
-    HTTP_STATUS_CODE: int = status_codes.HTTP_418_IM_A_TEAPOT
-
-    def run_validations(self, *, raise_exception: Optional[bool] = False) -> None:
+    def run_validations(self, *, raise_exception: Optional[bool] = False, **kwargs: Any) -> None:
         """
         Runs validations and registers errors/validated-data.
         If `raise_exception=True` and validations fail, raises `valcheck.apis.exceptions.ApiRequestValidationException`.
+
+        Accepted kwargs:
+            - http_status_code (int): The HTTP status code to use if `valcheck.apis.exceptions.ApiRequestValidationException` is raised. Default: 418.
         """
         super().run_validations()
         if raise_exception and self.errors:
+            http_status_code: int = kwargs.get("http_status_code", status_codes.HTTP_418_IM_A_TEAPOT)
             raise ApiRequestValidationException(
-                http_status_code=self.HTTP_STATUS_CODE,
+                http_status_code=http_status_code,
                 errors=self.errors,
             )
 
