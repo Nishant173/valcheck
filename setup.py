5b14dd6ba24b21d74902aab13aa7610b48acd30f
Nishant Rao <nishant.rao173@gmail.com>
2024-10-27 17:07:13 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 # Constants
 PACKAGE_NAME = "valcheck"
-PACKAGE_VERSION = "5.0"
+PACKAGE_VERSION = "6.0"
 AUTHOR_NAME = "Nishant Rao"
 AUTHOR_EMAIL_ID = "nishant.rao173@gmail.com"
 FILEPATH_TO_README = os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md")
